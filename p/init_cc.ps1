083c9070306ce415a506152edfcc3a613d93b308
ahominiddev <ahominid.dev@proton.me>
2024-09-29 17:26:03 +0800

    .

--- p/init_cc.ps1
+++ p/init_cc.ps1
@@ -51,6 +51,6 @@ Remove-Item -Recurse -Force $TempDir
 # 8. Replace the main function in the new project's main.dart
 $MainDartPath = "$RootDir\$NewProjectName\lib\main.dart"
 Write-Host "Modifying main.dart in the new project..."
-(Get-Content $MainDartPath) -replace 'void main\(\) \{[\s\S]*?runApp\(const MyApp\(\)\);\s*\}', 'void main() {`n  runApp(const ProviderScope(child: MyApp()));`n}' | Set-Content -Path $MainDartPath
+(Get-Content $MainDartPath) -replace 'void main\(\) \{[\s\S]*?runApp\(const MyApp\(\)\);\s*\}', 'void main() {`n  runApp(const ProviderScope(child: const MyApp()));`n}' | Set-Content -Path $MainDartPath
 
 Write-Host "Setup completed successfully."
--- s/init_cc.sh
+++ s/init_cc.sh
@@ -42,4 +42,9 @@ cp -r "$TEMP_DIR/lib/providers/"* "$ROOT_DIR/lib/providers/"
 echo "Deleting the temporary directory..."
 rm -rf "$TEMP_DIR"
 
+# 8. Replace the main function in the new project's main.dart
+MAIN_DART_PATH="$ROOT_DIR/$NEW_PROJECT_NAME/lib/main.dart"
+echo "Modifying main.dart in the new project..."
+sed -i 's|void main() {.*runApp(const MyApp());.*}|void main() {\n  runApp(const ProviderScope(child: const MyApp()));\n}|' "$MAIN_DART_PATH"
+
 echo "Setup completed successfully."
